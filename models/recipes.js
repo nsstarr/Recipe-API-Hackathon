286d441e8aa9f74a01eda51d35bc5b7339ddf7dc
……… <s@users-MacBook-Air.local>
2022-05-20 15:04:52 +0100

    importing modules

--- models/recipes.js
+++ models/recipes.js
@@ -1,16 +1,76 @@
 import recipes from "../libs/recipes.js";
 
+import recipesRouter from "../router/recipes.js"
+
 // GET ALL RECIPES
-export function getRecipes() {}
+ export function getRecipes() { 
+    recipesRouter.get("/", function (req, res) {
+    const responseObject = {success: true, payload: recipes}
+    res.json(responseObject)
+    }
+  ) 
+}
+
 
 // GET A RECIPE BY ID
-export function getRecipeByID(id) {}
+export function getRecipeByID(id) { 
+    recipesRouter.get("/:id", function(req, res) {
+       const id = req.params.id 
+       let searchedRecipe = {}
+       for (let i=0; i < recipes.length; i++) {
+          if (Number(id) === recipes[i].id) {
+          searchedRecipe = recipes[i]
+          }
+        }
+        const responseObject = {success: true, payload: searchedRecipe}
+        res.json(responseObject)
+        }
+    )
+}
 
 // CREATE A RECIPE
-export function createRecipe(newRecipe) {}
+export function createRecipe(newRecipe) {
+    recipesRouter.post(`/`, function(req, res) {
+       const body = req.body; 
+       recipes.push(body);
+       const responseObject = {
+         success: true,
+         payload: recipes} 
+         res.json(responseObject)
+    }
+    )
+}
 
 // UPDATE A RECIPE BY ID
-export function updateRecipeByID(id, updatedRecipe) {}
+export function updateRecipeByID(id, updatedRecipe) {
+    recipesRouter.post(`/`, function(req, res) {
+    const body = req.body; 
+    recipes.push(body);
+    const responseObject = {
+      sucess: true,
+      payload: recipes} 
+      res.json(responseObject)
+    }
+    )
+}
 
 // DELETE A RECIPE BY ID
-export function deleteRecipeByID(id) {}
+export function deleteRecipeByID(id) {
+    recipesRouter.delete('/:id', function (req, res) {
+    const id = req.params.id;
+    let deletedRecipe = {}
+    for (let i = 0; i < recipes.length; i++) {
+      if (Number(id) === recipes[i].id) {
+       deletedRecipe = recipes.splice(recipes[i])
+       break}  }
+      const responseObject = {success: true, payload: deletedRecipe}
+      res.json(responseObject)
+      }
+    )
+}
+
+
+/* PLAN
+-  bring all the code from router into this file 
+-  import each function into router file 
+ */
--- router/recipes.js
+++ router/recipes.js
@@ -2,41 +2,38 @@
 import express from "express";
 const recipesRouter = express.Router();
 
-import recipes from '../libs/recipes.js';
+//import recipes from '../libs/recipes.js';
+
+let express = express()
+
+express.use(express.json()
 
 
-recipesRouter.get("/", function (req, res){
-    const responseObject = {success: true, payload: recipes}
-    res.json(responseObject)
-}
-)
 
-export default recipesRouter;
+import {getRecipes} from "../models/recipes.js"
 
-recipesRouter.get("/:id", function(req, res) {
-    const id = req.params.id 
-    let searchedRecipe = {}
-    for (let i=0; i < recipes.length; i++) {
-        if (Number(id) === recipes[i].id) {
-          searchedRecipe = recipes[i]
-        }
-      }
-      const responseObject = {success: true, payload: searchedRecipe}
-        res.json(responseObject)
-    
-})
+recipesRouter.use("/",getRecipes())
 
-recipesRouter.post(`/`, function(req, res) {
-  const body = req.body; 
-  recipes.push(body);
-  const responseObject = {
-    sucess: true,
-    payload: recipes} 
-    res.json(responseObject)
-  })
+import {getRecipeByID} from "../models/recipes.js"
+
+recipesRouter.use(":/id",getRecipeByID())
+
+import {createRecipe} from "../models/recipes.js"
+
+recipesRouter.use("/",createRecipe())
+
+import {updateRecipeByID} from "../models/recipes.js"
+
+recipesRouter.use(":/if",updateRecipeByID())
+
+import {deleteRecipeByID} from "../models/recipes.js"
+
+recipesRouter.use(":/id",deleteRecipeByID())
 
 
-  /*
+
+
+/*
   - update our recipe in postman 
   - we're not adding another item to the array -> XXXX array.push 
   - use id of the recipe
@@ -45,24 +42,7 @@ recipesRouter.post(`/`, function(req, res) {
   - replace with updated recipe
    */
 
-  recipesRouter.put('/:id', function(req, res) {
-    const id = req.params.id;
-    console.log(id)
-    const body = req.body;
-    console.log(body)
-    let searchedRecipe = {}
-    console.log(searchedRecipe)
-    for (let i = 0; i < recipes.length; i++) {
-        if (Number(id) === recipes[i].id) {
-            recipes[i] = body
-            searchedRecipe = recipes[i]
-            break
-        }
-    }
-    console.log(recipes)
-        const responseObject = {success: true, payload: searchedRecipe}
-        res.json(responseObject)
-  })
+ 
 
   /*
   - Iterate through the loop to find an id of the recipe we want to remove
@@ -70,13 +50,3 @@ recipesRouter.post(`/`, function(req, res) {
   - Update responseObject
   - No need to use express.json() to delete requests
   */
-recipesRouter.delete('/:id', function (req, res) {
-  const id = req.params.id;
-  let deletedRecipe = {}
-  for (let i = 0; i < recipes.length; i++) {
-    if (Number(id) === recipes[i].id) {
-     deletedRecipe = recipes.splice(recipes[i])
-     break}  }
-    const responseObject = {success: true, payload: deletedRecipe}
-    res.json(responseObject)
-    })
